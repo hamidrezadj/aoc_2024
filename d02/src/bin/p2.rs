6d934e825014deb19334aaadbca9301bf7ddfbfc
hamidrezadj <hamidrezadj.dy7mq@aleeas.com>
2024-12-18 11:06:42 +0330

    Add day 2 part 2 solution

---
+++ d02/src/bin/p2.rs
@@ -0,0 +1,88 @@
+use std::cmp::Ordering;
+
+fn main() {
+    let safe_entry_count = std::io::stdin()
+        .lines()
+        .map(|line_result| line_result.expect("Stdin error"))
+        .map(|line| {
+            line.split_whitespace()
+                .map(|word| {
+                    word.parse::<u32>()
+                        .expect("Not an unsigned integer that fits in 32 bits")
+                })
+                .collect::<Vec<u32>>()
+        })
+        .map(|entry| {
+            let first_number = entry.first().expect("Line empty");
+            let last_number = entry.iter().last().expect("Line empty");
+            let ordering = last_number.cmp(first_number);
+            (entry, ordering)
+        })
+        .map(|(entry, ordering)| {
+            let mut problematic_idx = 0usize;
+            let problematic_option =
+                match entry
+                    .windows(2)
+                    .enumerate()
+                    .all(|(idx, window)| match ordering {
+                        Ordering::Less
+                            if window[0] > window[1]
+                                && (1..=3).contains(&(window[0] - window[1])) =>
+                        {
+                            true
+                        }
+                        Ordering::Greater
+                            if window[0] < window[1]
+                                && (1..=3).contains(&(window[1] - window[0])) =>
+                        {
+                            true
+                        }
+                        _ => {
+                            problematic_idx = idx;
+                            false
+                        }
+                    }) {
+                    true => None,
+                    false => Some(problematic_idx),
+                };
+            (entry, ordering, problematic_option)
+        })
+        .filter(|(entry, ordering, problematic_idx)| match problematic_idx {
+            None => true,
+            Some(idx) => {
+                let mut try_1 = entry.clone();
+                try_1.remove(*idx);
+                let is_try_1_good = try_1.windows(2).all(|window| match ordering {
+                    Ordering::Less
+                        if window[0] > window[1] && (1..=3).contains(&(window[0] - window[1])) =>
+                    {
+                        true
+                    }
+                    Ordering::Greater
+                        if window[0] < window[1] && (1..=3).contains(&(window[1] - window[0])) =>
+                    {
+                        true
+                    }
+                    _ => false,
+                });
+                let mut try_2 = entry.clone();
+                try_2.remove(*idx + 1);
+                let is_try_2_good = try_2.windows(2).all(|window| match ordering {
+                    Ordering::Less
+                        if window[0] > window[1] && (1..=3).contains(&(window[0] - window[1])) =>
+                    {
+                        true
+                    }
+                    Ordering::Greater
+                        if window[0] < window[1] && (1..=3).contains(&(window[1] - window[0])) =>
+                    {
+                        true
+                    }
+                    _ => false,
+                });
+                is_try_1_good || is_try_2_good
+            }
+        })
+        .count();
+    println!("{}", safe_entry_count);
+}
