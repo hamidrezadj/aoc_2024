c605b377cf22ac15f5674a44f5bcfb2264eb50f3
hamidrezadj <hamidrezadj.dy7mq@aleeas.com>
2024-12-18 12:50:19 +0330

    Fix a logic error in day 2 part 2 solution
    Since the ordering (ascending or descending status) of an entry is
    caclucalted based on the first and last number of an entry, then when
    one of them is considered problematic and removed, the ordering has to
    be calculated again.

--- d02/src/bin/p2.rs
+++ d02/src/bin/p2.rs
@@ -44,13 +44,16 @@ fn main() {
                             Err(problematic_idxs)
                         }
                     });
-            (entry, ordering, problematic_idxs)
+            (entry, problematic_idxs)
         })
-        .filter(|(entry, ordering, problematic_idx)| match problematic_idx {
+        .filter(|(entry, problematic_idx)| match problematic_idx {
             Ok(()) => true,
             Err(problematic_idxs) => problematic_idxs.iter().any(|problematic_idx| {
                 let mut entry = entry.clone();
                 entry.remove(*problematic_idx);
+                let first_number = entry.first().expect("Line empty");
+                let last_number = entry.last().expect("Line empty");
+                let ordering = last_number.cmp(first_number);
                 entry.windows(2).all(|window| match ordering {
                     Ordering::Less if is_window_safely_descending(window) => true,
                     Ordering::Greater if is_window_safely_ascending(window) => true,
