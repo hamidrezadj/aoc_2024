45f40d6bc9cda9f7046426966f48d95462d8307f
hamidrezadj <hamidrezadj.dy7mq@aleeas.com>
2024-12-19 09:06:55 +0330

    Fix edge case handling in day 2 part 2 solution

--- d02/src/bin/p2.rs
+++ d02/src/bin/p2.rs
@@ -13,13 +13,18 @@ fn main() {
                 .collect::<Vec<u32>>()
         })
         .inspect(|entry| {
-            if entry.len() < 2 {
-                panic!("Less than two numbers in line");
+            if entry.is_empty() {
+                panic!("Line empty");
             }
         })
         .map(|entry| {
-            let first_number = entry.first().expect("Line empty");
-            let last_number = entry.last().expect("Line empty");
+            if entry.len() == 1 {
+                // Safety of an entry with one number is ambiguous.
+                // In this implementation they are considered safe.
+                return (entry, Ok(()));
+            }
+            let first_number = entry.first().unwrap();
+            let last_number = entry.last().unwrap();
             let ordering = last_number.cmp(first_number);
             let entry_safety_result =
                 entry
@@ -51,14 +56,16 @@ fn main() {
         .filter(|(entry, entry_safety_result)| match entry_safety_result {
             Ok(()) => true,
             Err(problematic_idxs) => problematic_idxs.iter().any(|problematic_idx| {
-                if entry.len() < 3 {
-                    // Fixing an entry that has less than 3 numbers is ambiguous.
-                    eprintln!("Warning: Fixing an entry with only 2 numbers always makes it safe");
+                if entry.len() == 2 {
+                    // Fixing an entry that has two numbers results in an
+                    // entry with one number in it, which its safety is ambiguous.
+                    // In this implementation they are considered safe.
+                    return true;
                 }
                 let mut entry = entry.clone();
                 entry.remove(*problematic_idx);
-                let first_number = entry.first().expect("Line empty");
-                let last_number = entry.last().expect("Line empty");
+                let first_number = entry.first().unwrap();
+                let last_number = entry.last().unwrap();
                 let ordering = last_number.cmp(first_number);
                 entry.windows(2).all(|window| match ordering {
                     Ordering::Less if is_window_safely_descending(window) => true,
