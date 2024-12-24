4782f3c780ef65869226ac9e91da7bc44a09322c
hamidrezadj <hamidrezadj.dy7mq@aleeas.com>
2024-12-24 20:15:25 +0330

    Add day 8 part 1 solution

---
+++ d08/src/bin/p1.rs
@@ -0,0 +1,56 @@
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    let input: Vec<Vec<char>> = std::io::stdin()
+        .lines()
+        .map(|line_result| line_result.expect("Stdin error"))
+        .map(|line| line.chars().collect())
+        .collect();
+    assert!(!input.is_empty(), "Empty input");
+    assert!(!input[0].is_empty(), "Empty line");
+    assert!(
+        input.iter().all(|row| row.len() == input[0].len()),
+        "Uneven input"
+    );
+    let max_i = input.len() as i64 - 1;
+    let max_j = input[0].len() as i64 - 1;
+    let is_in_bound = |(i, j)| i >= 0 && i <= max_i && j >= 0 && j <= max_j;
+    let antennas: HashMap<char, Vec<(i64, i64)>> = input
+        .into_iter()
+        .enumerate()
+        .flat_map(|(i, row)| {
+            row.into_iter()
+                .enumerate()
+                .map(move |(j, ch)| (ch, (i as i64, j as i64)))
+        })
+        .filter(|(ch, _pos)| ch.is_alphanumeric())
+        .fold(HashMap::new(), |mut acc, (ch, pos)| {
+            acc.entry(ch).or_default().push(pos);
+            acc
+        });
+    let anti_nodes: HashSet<(i64, i64)> = antennas
+        .into_values()
+        .flat_map(combinations_of_two)
+        .flat_map(|((a1i, a1j), (a2i, a2j))| {
+            let diff_i = a1i - a2i;
+            let diff_j = a1j - a2j;
+            let anti_node_1 = (a1i + diff_i, a1j + diff_j);
+            let anti_node_2 = (a2i - diff_i, a2j - diff_j);
+            [anti_node_1, anti_node_2]
+                .into_iter()
+                .filter(|node| is_in_bound(*node))
+        })
+        .collect();
+    let output = anti_nodes.len();
+    println!("{}", output);
+}
+
+fn combinations_of_two(positions: Vec<(i64, i64)>) -> Vec<((i64, i64), (i64, i64))> {
+    let mut combinations = Vec::new();
+    for i in 0..positions.len() {
+        for j in (i + 1)..positions.len() {
+            combinations.push((positions[i], positions[j]));
+        }
+    }
+    combinations
+}
