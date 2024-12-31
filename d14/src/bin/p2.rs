6ca249685104ff5a0109f48d7d46a3080f0f97f6
hamidrezadj <hamidrezadj.dy7mq@aleeas.com>
2024-12-31 17:48:33 +0330

    Add more features to day 14 part 2 solution

--- d14/src/bin/p2.rs
+++ d14/src/bin/p2.rs
@@ -1,6 +1,9 @@
 use std::{collections::BinaryHeap, env, io};
 
 fn main() {
+    let mut args = env::args().skip(1).take(2);
+    let control_argument = args.next();
+    let target_time = args.next();
     let x_len = env::var("X_LEN")
         .map(|x| {
             x.parse::<i64>()
@@ -45,50 +48,68 @@ fn main() {
             ((px, py), (vx, vy))
         })
         .collect();
-    let mut input = input;
-    for t in 1..=20000 {
-        let mut buffer: BinaryHeap<(i64, i64)> = BinaryHeap::new();
-        input.iter_mut().for_each(|((px, py), (vx, vy))| {
-            *px = px.checked_add(*vx).expect("Overflow").rem_euclid(x_len);
-            *py = py.checked_add(*vy).expect("Overflow").rem_euclid(y_len);
-            buffer.push((*py, *px));
-        });
-        println!();
-        for _ in 0..x_len {
-            print!("-");
+    match control_argument {
+        Some(ca) if ca == "draw-until" => {
+            let target_time = target_time
+                .expect("No time target argument passed")
+                .parse::<i64>()
+                .expect("Time target argument not an integer that fits in 64 bits");
+            let mut input = input;
+            for time in 1..=target_time {
+                let mut buffer: BinaryHeap<(i64, i64)> = BinaryHeap::new();
+                input.iter_mut().for_each(|((px, py), (vx, vy))| {
+                    *px = px.checked_add(*vx).expect("Overflow").rem_euclid(x_len);
+                    *py = py.checked_add(*vy).expect("Overflow").rem_euclid(y_len);
+                    buffer.push((*py, *px));
+                });
+                draw(x_len, time, buffer);
+            }
         }
-        println!();
-        println!("t={}", t);
-        println!();
-        println!();
-        println!();
-        println!();
-        let mut current_y = 0;
-        let mut current_x = 0;
-        buffer.into_sorted_vec().into_iter().for_each(|(y, x)| {
-            if (x, y) == (current_x, current_y) {
-                return;
-            }
-            if y > current_y {
-                current_x = 0;
-                for _ in current_y..y {
-                    println!();
-                }
-                current_y = y;
-            }
-            for _ in current_x..x - 1 {
-                print!("   ");
-            }
-            print!("###");
-            current_x = x;
-        });
-        println!();
-        println!();
-        println!();
-        println!();
-        for _ in 0..x_len {
-            print!("-");
+        Some(ca) if ca == "draw" => {
+            let time = target_time
+                .expect("No time target argument passed")
+                .parse::<i64>()
+                .expect("Time target argument not an integer that fits in 64 bits");
+            let buffer: BinaryHeap<(i64, i64)> = input
+                .iter()
+                .map(|((px, py), (vx, vy))| {
+                    let dx = vx.checked_mul(time).expect("Overflow");
+                    let dy = vy.checked_mul(time).expect("Overflow");
+                    let x = px.checked_add(dx).expect("Overflow").rem_euclid(x_len);
+                    let y = py.checked_add(dy).expect("Overflow").rem_euclid(y_len);
+                    (y, x)
+                })
+                .collect();
+            draw(x_len, time, buffer);
         }
-        println!();
+        Some(_) => panic!("Invalid argument: Either use 'draw-until time' or 'draw time'"),
+        None => println!("{}", 103 * 75 + 65),
     }
+}
+
+fn draw(x_len: i64, time: i64, buffer: BinaryHeap<(i64, i64)>) {
+    println!("{}", "---".repeat(x_len as usize));
+    println!("t={}", time);
+    print!("\n\n\n\n");
+    let mut current_y = 0;
+    let mut current_x = 0;
+    buffer.into_sorted_vec().into_iter().for_each(|(y, x)| {
+        if (x, y) == (current_x, current_y) {
+            return;
+        }
+        if y > current_y {
+            current_x = 0;
+            for _ in current_y..y {
+                println!();
+            }
+            current_y = y;
+        }
+        for _ in current_x..x - 1 {
+            print!("   ");
+        }
+        print!("###");
+        current_x = x;
+    });
+    print!("\n\n\n\n");
+    println!("{}", "---".repeat(x_len as usize));
 }
