395c64d469d015364d703ccb63f00d0752ff051e
hamidrezadj <hamidrezadj.dy7mq@aleeas.com>
2025-01-04 18:00:58 +0330

    Add some optimizations for day 16 part 1

--- d16/src/bin/p1.rs
+++ d16/src/bin/p1.rs
@@ -101,45 +101,58 @@ fn main() {
 
 fn neighbours(input: &[Vec<char>], node: Node, length: u64) -> BTreeSet<(u64, Node)> {
     let next_node_in_same_direction = Some(node.position)
-        .map(|(i, j)| match node.direction {
-            Direction::East => (i, j + 1),
-            Direction::South => (i + 1, j),
-            Direction::West => (i, j - 1),
-            Direction::North => (i - 1, j),
-        })
+        .into_iter()
+        .map(|position| node.direction.forward_position(position))
         .filter(|&(i, j)| input[i][j] != '#')
         .map(|position| Node {
             position,
             direction: node.direction,
         })
-        .map(|node| (length + 1, node))
-        .into_iter();
-
-    [
-        Direction::East,
-        Direction::South,
-        Direction::West,
-        Direction::North,
-    ]
-    .into_iter()
-    .filter(|dir| *dir != node.direction)
-    .filter(|dir| *dir != node.direction.opposite())
-    .map(|dir| Node {
-        position: node.position,
-        direction: dir,
-    })
-    .map(|node| (length + 1000, node))
-    .chain(next_node_in_same_direction)
-    .collect()
+        .map(|node| (length + 1, node));
+    let rotated_nodes = Some(node.position)
+        .into_iter()
+        .flat_map(|position| node.direction.side_positions(position))
+        .filter(|&(i, j)| input[i][j] != '#')
+        .map(|side_position| Direction::find_direction(side_position, node.position))
+        .map(|direction| Node {
+            position: node.position,
+            direction,
+        })
+        .map(|node| (length + 1000, node));
+    next_node_in_same_direction.chain(rotated_nodes).collect()
 }
 
 impl Direction {
-    fn opposite(&self) -> Self {
+    fn forward_position(&self, position: (usize, usize)) -> (usize, usize) {
+        let (i, j) = position;
         match self {
-            Direction::East => Direction::West,
-            Direction::South => Direction::North,
-            Direction::West => Direction::East,
-            Direction::North => Direction::South,
+            Direction::East => (i, j + 1),
+            Direction::South => (i + 1, j),
+            Direction::West => (i, j - 1),
+            Direction::North => (i - 1, j),
+        }
+    }
+    fn side_positions(&self, position: (usize, usize)) -> [(usize, usize); 2] {
+        let (i, j) = position;
+        match self {
+            Direction::East | Direction::West => [(i + 1, j), (i - 1, j)],
+            Direction::South | Direction::North => [(i, j + 1), (i, j - 1)],
+        }
+    }
+
+    fn find_direction(position2: (usize, usize), position1: (usize, usize)) -> Direction {
+        let (i2, j2) = position2;
+        let (i1, j1) = position1;
+        if i2 == i1 && j2 == j1 + 1 {
+            Direction::East
+        } else if i2 == i1 + 1 && j2 == j1 {
+            Direction::South
+        } else if i2 == i1 && j2 + 1 == j1 {
+            Direction::West
+        } else if i2 + 1 == i1 && j2 == j1 {
+            Direction::North
+        } else {
+            panic!("Not a proper use of find_direction")
         }
     }
 }
