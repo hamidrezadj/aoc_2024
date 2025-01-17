fe7831327b55e93db644f08af2e4fffceb0611db
hamidrezadj <hamidrezadj.dy7mq@aleeas.com>
2025-01-17 10:43:20 +0330

    Parallelize day 22 part 2 solution

--- d22/src/bin/p2.rs
+++ d22/src/bin/p2.rs
@@ -1,50 +1,59 @@
+use itertools::Itertools;
+use rayon::prelude::*;
 use std::{
     collections::{HashMap, HashSet},
     ops::AddAssign,
+    sync::mpsc,
 };
 
 fn main() {
-    let results: HashMap<Vec<i64>, u64> = std::io::stdin()
+    let input: Vec<i64> = std::io::stdin()
         .lines()
         .map(|line_result| line_result.expect("Stdin error"))
         .map(|line| {
             line.parse::<u64>()
-                .expect("Not an unsigned integer that fits in 64 bits")
+                .expect("Not an unsigned integer that fits in 64 bits") as i64
         })
-        .fold(HashMap::new(), |mut acc, initial_n| {
-            let prices: Vec<u64> = (0..2000)
-                .scan(initial_n, |n, _idx| {
-                    *n = ((*n * 64) ^ *n) % 16777216;
-                    *n = ((*n / 32) ^ *n) % 16777216;
-                    *n = ((*n * 2048) ^ *n) % 16777216;
-                    Some(*n)
-                })
-                .map(|n| n % 10)
-                .collect();
-            let changes: Vec<i64> = prices
-                .iter()
-                .scan(initial_n % 10, |prev_n, curr_n| {
-                    let change = *curr_n as i64 - *prev_n as i64;
-                    *prev_n = *curr_n;
-                    Some(change)
-                })
-                .collect();
-            changes
-                .windows(4)
-                .zip(prices.into_iter().skip(3))
-                .scan(HashSet::new(), |seen_changes, (window, price)| {
-                    if seen_changes.contains(&window) {
-                        return Some(None);
-                    }
-                    seen_changes.insert(window);
+        .collect();
+    let (sender, receiver) = mpsc::channel();
+    input.into_par_iter().for_each(|initial_n| {
+        (0..2000)
+            .scan(initial_n, |n, _idx| {
+                *n = ((*n * 64) ^ *n) % 16777216;
+                *n = ((*n / 32) ^ *n) % 16777216;
+                *n = ((*n * 2048) ^ *n) % 16777216;
+                Some(*n)
+            })
+            .map(|n| n % 10)
+            .scan(initial_n % 10, |prev_price, curr_price| {
+                let change = *prev_price - curr_price;
+                *prev_price = curr_price;
+                Some((change, curr_price))
+            })
+            .tuple_windows()
+            .map(|((c1, _), (c2, _), (c3, _), (c4, price))| ([c1, c2, c3, c4], price))
+            .scan(HashSet::new(), |seen_windows, (window, price)| {
+                let was_not_seen_before = seen_windows.insert(window);
+                if was_not_seen_before {
                     Some(Some((window, price)))
-                })
-                .flatten()
-                .for_each(|(window, price)| {
-                    acc.entry(window.to_vec()).or_insert(0).add_assign(price)
-                });
+                } else {
+                    Some(None)
+                }
+            })
+            .flatten()
+            .for_each(|(window, price)| {
+                sender.send((window, price)).unwrap();
+            });
+    });
+    drop(sender);
+    let output = receiver
+        .iter()
+        .fold(HashMap::new(), |mut acc, (window, price)| {
+            acc.entry(window).or_insert(0).add_assign(price);
             acc
-        });
-    let output = results.into_values().max().expect("Empty input");
+        })
+        .into_values()
+        .max()
+        .expect("Empty input");
     println!("{}", output);
 }
